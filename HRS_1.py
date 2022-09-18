3e3df54a042ddba5a84c26933697a86c7cbd1db7
al-ruhanyat <alruhanyat994@gmail.com>
2022-09-18 19:49:56 +0600

    HRS(1-2)

---
+++ HRS_1.py
@@ -0,0 +1,14 @@
+n=int(input())
+
+remainder = n%2
+
+if remainder!=0:
+    print("Weird")
+    2
+elif remainder==0 and 2<=n<=5:
+    print("Not Weird")
+elif remainder==0 and 6<=n<=20:
+    print("Weird")
+elif remainder==0 and n>20:
+    print("Not Weird")
+    
